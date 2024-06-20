64438cffa0a208d2d98b4f69581ed529aab849b3
iishanibhatt <102678305+iishanibhatt@users.noreply.github.com>
2024-06-20 22:44:43 +0530

    register dto

---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs;
+
+public class RegisterDto{
+    [Required]
+    public string username { get; set; }
+    [Required]
+    public string password { get; set; }
+}
